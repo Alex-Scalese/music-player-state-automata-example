0ee60ae5a4aa3deff749462861c57a2116560978
alex scalese <mail@alex-scalese.de>
2019-02-13 15:43:22 +0100

    Add State Interface for MusicPlayer

--- include/States.hpp
+++ include/States.hpp
@@ -5,4 +5,14 @@
 #ifndef MUSICPLAYER_STATE_EXAMPLE_STATES_HPP
 #define MUSICPLAYER_STATE_EXAMPLE_STATES_HPP
 
+class MusicPlayer;
+
+class States {
+
+public:
+    virtual void Play(MusicPlayer &player) = 0;
+    virtual void Stop(MusicPlayer &player) = 0;
+    virtual void Pause(MusicPlayer &player) = 0;
+};
+
 #endif //MUSICPLAYER_STATE_EXAMPLE_STATES_HPP
